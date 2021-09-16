30417e42e336a7970333888a5b6340f9ec265465
Anmol <bhatianmol566@yahoo.co.uk>
2021-09-16 21:24:02 +0100

    Add students to directory.rb

--- directory.rb
+++ directory.rb
@@ -1,13 +1,20 @@
 puts "The students of Villains Academy"
 puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex DeLarge"
-puts "The Wicked Witch of the West"
-puts "Terminator"
-puts "Freddy Krueger"
-puts "The Joker"
-puts "Joffrey Baratheon"
-puts "Norman Bates"
+
+students = [
+  "Dr. Hannibal Lecter",
+  "Darth Vader",
+  "Nurse Ratched",
+  "Michael Corleone",
+  "Alex DeLarge",
+  "The Wicked Witch of the West",
+  "Terminator",
+  "Freddy Krueger",
+  "The Joker",
+  "Joffrey Baratheon",
+  "Norman Bates"
+]
+
+puts students
+puts "Altogether, we have #{students.size} students!"
+# students.each{|student| puts student}
